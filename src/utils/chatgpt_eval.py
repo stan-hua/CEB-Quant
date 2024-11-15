32c19b8cb713a3fac1b1dc4b5bf9b02f2639a30c
stan-hua <stanley.z.hua@gmail.com>
2024-11-14 23:06:54 -0500

    Implement ChatGPTGenerator for performing inference

--- src/utils/chatgpt_eval.py
+++ src/utils/chatgpt_eval.py
@@ -25,65 +25,32 @@ DEFAULT_MODEL = "gpt-4o-2024-08-06"
 ################################################################################
 #                                   Classes                                    #
 ################################################################################
-@retry(wait=wait_random_exponential(min=1, max=120), stop=stop_after_attempt(6))
-def get_res(string, model=DEFAULT_MODEL, temperature=0, message=None):
-    """
-    Retrieve a response from the OpenAI ChatCompletion API.
-
-    Args:
-        string (str): The input string to process.
-        model (str): The model to use for generating the response. Default is DEFAULT_MODEL.
-        temp (float): The temperature setting for the API request. Default is 0 for deterministic output.
-
-    Returns:
-        str: The API response content.
-
-    Raises:
-        ValueError: If the API response is null or an empty string.
-    """
-    try:
-        if message is None:
-            message = [{"role": "user", "content": string}]
-
-        api_key = config.OPENAI_KEY
-        if config.OPENAI_API_URL is not None:
-            client = OpenAI(
-                api_key=api_key,
-                base_url=config.OPENAI_API_URL
-            )
-        else:
-            client = OpenAI(api_key=api_key)
-        # Temperature will be set to 0 for deterministic output (i.e., greedy decoding)
-        stream = client.chat.completions.create(model=model,
-                                                messages=message,
-                                                temperature=temperature,)
-        if not stream.choices[0].message.content:
-            raise ValueError("The response from the API is NULL or an empty string!")
-        response = stream.choices[0].message.content
-    except Exception as e:
-        print(e)
-        return None
-    return response
-
-
 class ChatGPTEvaluator:
     """
-    A class for automating the evaluation of text using the OpenAI API.
+    ChatGPTEvaluator class.
+
+    Notes
+    -----
+    Used to evaluate LLM responses via the OpenAI Chat Completion API
     """
 
-    def __init__(self, model=DEFAULT_MODEL, save_dir=config.DIR_EVALUATIONS):
+    def __init__(self, model=DEFAULT_MODEL, save_dir=None):
         """
-        Initialize the AutoEvaluator class.
+        Initialize the ChatGPTEvaluator class.
 
-        Args:
-            save_dir (str): Directory for saving evaluation results.
+        Parameters
+        ----------
+        model : str, optional
+            The OpenAI model to be used for evaluation, by default DEFAULT_MODEL.
+        save_dir : str, optional
+            The directory to save evaluation results. Defaults to a directory
+            within config.DIR_EVALUATIONS based on the model name.
         """
         self.model = model
-        self.save_dir = save_dir
+        self.save_dir = save_dir or os.path.join(config.DIR_EVALUATIONS, model)
         self.max_worker = config.MAX_WORKER_AUTOEVAL
         if not os.path.exists(self.save_dir):
             os.makedirs(self.save_dir)
-        # openai.api_key = config.openai_key
 
 
     def save_progress(self, data, filename='auto_eval.json', **save_kwargs):
@@ -137,10 +104,10 @@ class ChatGPTEvaluator:
                 LOGGER.error("An error occurred: %s", str(future.exception()))
                 self.save_progress(data, filename=progress_filename)
 
-        def process_item(item, row):
+        def process_row(item, row):
             try:
                 if not row.get(llm_response_col):
-                    llm_response = get_res(item, model=self.model)
+                    llm_response = openai_chat_completion(item, model=self.model)
                     row[llm_response_col] = llm_response
             except Exception as error_msg:
                 raise error_msg
@@ -149,35 +116,8 @@ class ChatGPTEvaluator:
         if not data:
             return []
 
-        # Set up prompt formatters
-        task_to_prompt = config.TASK_TO_PROMPT_DICT
-        # If prompt contains row formatters, then fill them in with row information
-        task_prompt_dict = task_to_prompt.get(task, {})
-        use_prompt_formatter = "mapping" in task_prompt_dict
-
-        # Prepare prompts
-        # CASE 1: Prompt contains string formatters
-        prompts = []
-        if use_prompt_formatter:
-            replace_dict = task_prompt_dict.get('mapping', {})
-            prompt = task_prompt_dict.get('prompt', '')
-            for row in data:
-                single_prompt = prompt
-                for k, v in replace_dict.items():
-                    # CASE 1: If "res" was specified, but LLM input column is different
-                    #         then convert
-                    if v == "res" and llm_input_col != "res":
-                        val = row[llm_input_col]
-                    # CASE 2: Any other column
-                    else:
-                        val = row[v]
-                    single_prompt = single_prompt.replace(k, str(val))
-                prompts.append(single_prompt)
-        # CASE 2: Otherwise, simply append LLM response to end of prompt
-        else:
-            LOGGER.debug("[ChatGPT Evaluator] Concatenating LLM response to prompt")
-            prompt = task_prompt_dict.get('prompt', '')
-            prompts = [prompt + item[llm_input_col] for item in data]
+        # Prepare prompts for evaluating LLM responses
+        prompts = prepare_llm_eval_prompts(data, task, llm_input_col)
 
         # If specified, resume from previous evaluation
         if resume:
@@ -200,7 +140,7 @@ class ChatGPTEvaluator:
 
         # Perform LLM generation requests in parallel
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_worker) as executor:
-            futures = [executor.submit(process_item, item, row) for item, row in zip(prompts, data)]
+            futures = [executor.submit(process_row, item, row) for item, row in zip(prompts, data)]
 
             # Add a callback to handle completion and errors
             for idx, future in enumerate(concurrent.futures.as_completed(futures)):
@@ -215,3 +155,249 @@ class ChatGPTEvaluator:
         self.save_progress(data, filename=progress_filename)
 
         return data
+
+
+class ChatGPTGenerator:
+    """
+    ChatGPTGenerator class.
+
+    Notes
+    -----
+    Used to perform generation via the OpenAI Chat Completion API
+    """
+
+    def __init__(self, model=DEFAULT_MODEL, save_dir=None):
+        """
+        Initialize the ChatGPTGenerator class.
+
+        Parameters
+        ----------
+        model : str, optional
+            The OpenAI model to be used for evaluation, by default DEFAULT_MODEL.
+        save_dir : str, optional
+            The directory to save evaluation results. Defaults to a directory
+            within config.DIR_GENERATIONS based on the model name.
+        """
+        self.model = model
+        self.save_dir = save_dir or os.path.join(config.DIR_GENERATIONS, model)
+        self.max_worker = config.MAX_WORKER_AUTOEVAL
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+
+    def save_progress(self, data, filename="chatgpt_gen.json", **save_kwargs):
+        """
+        Save progress to a JSON file.
+
+        Parameters
+        ----------
+            data : list of dict
+                Data to be saved.
+            filename : str
+                Filename to save date
+        """
+        save_path = os.path.join(self.save_dir, filename)
+        json_utils.save_json(data, save_path, **save_kwargs)
+
+
+    def infer(
+        self, data,
+        resume=True, progress_filename="infer_progress.json",
+        llm_input_col="prompt", llm_response_col="res",
+    ):
+        """
+        Perform inference on a dataset using the OpenAI API.
+
+        Parameters
+        ----------
+        data : list of dict
+            Each dict contains a prompt in the `llm_input_col` to perform
+            inference on.
+        resume : bool, optional
+            If True, then try to resume inference from a saved progress file
+            with the same filename as `progress_filename`. Default is True.
+        progress_filename : str, optional
+            Filename for saving or resuming progress.
+        llm_input_col : str, optional
+            Key to prompt to perform inference on
+        llm_response_col : str, optional
+            Key to store LLM's response.
+
+        Returns
+        -------
+        list
+            The evaluated data.
+        """
+        def save_progress_callback(future):
+            if future.exception() is not None:
+                LOGGER.error("An error occurred: %s", str(future.exception()))
+                self.save_progress(data, filename=progress_filename)
+
+        def process_row(item, row):
+            try:
+                if not row.get(llm_response_col):
+                    llm_response = openai_chat_completion(item, model=self.model)
+                    row[llm_response_col] = llm_response
+            except Exception as error_msg:
+                raise error_msg
+
+        # Early return, if no data provided
+        if not data:
+            return []
+
+        # Ensure all rows have a prompt
+        assert all(llm_input_col in row for row in data), "All rows must have a prompt specified!"
+
+        # Assume full prompt is specified in the llm input column
+        prompts = [data.get(llm_input_col) for data in data]
+
+        # If specified, resume from previous inference
+        if resume:
+            load_path = os.path.join(self.save_dir, progress_filename)
+            try:
+                prev_data = json_utils.load_json(load_path)
+                if prev_data and len(data) == len(prev_data):
+                    LOGGER.info("Resuming inference from saved progress.")
+                    data = prev_data
+            except FileNotFoundError:
+                LOGGER.warning("No saved progress file found at %s. Starting a new inference.", load_path)
+
+        # Perform input sanitization
+        assert isinstance(data, list), f"Data must be a list. data={data}"
+        assert data, "Data provided is empty!"
+
+        # Create thread lock
+        lock = threading.Lock()
+
+        # Perform LLM generation requests in parallel
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_worker) as executor:
+            futures = [executor.submit(process_row, item, row) for item, row in zip(prompts, data)]
+
+            # Add a callback to handle completion and errors
+            for idx, future in enumerate(concurrent.futures.as_completed(futures)):
+                future.add_done_callback(save_progress_callback)
+                if idx % 10 == 0:
+                    self.save_progress(data, filename=progress_filename, lock=lock)
+
+            # Wait for all futures to complete
+            concurrent.futures.wait(futures)
+
+        # Save progress
+        self.save_progress(data, filename=progress_filename)
+
+        return data
+
+
+################################################################################
+#                               Helper Functions                               #
+################################################################################
+@retry(wait=wait_random_exponential(min=1, max=120), stop=stop_after_attempt(6))
+def openai_chat_completion(text_or_msgs=None, model=DEFAULT_MODEL, temperature=0):
+    """
+    Sends string/messages from the OpenAI ChatCompletion API.
+
+    Parameters
+    ----------
+    text_or_msgs : str or list of dict, optional
+        The input user text to be processed by the API, or a list of messages
+        to be processed by the API.
+    model : str, optional
+        The model to use for the API request. Default is "gpt-4o-2024-08-06".
+    temperature : float, optional
+        The temperature to use for the API request. Default is 0.
+
+    Returns
+    -------
+    Union[None, str]
+        If the API response is null or an empty string, returns None.
+        Otherwise, returns the response from the API.
+    """
+    assert text_or_msgs, f"Please provide valid input text/messages! Received: `{text_or_msgs}`"
+    assert isinstance(text_or_msgs, (str, list)), f"Input text/messages must be either a str or a List[Dict]!"
+    try:
+        # Prepare input
+        messages = text_or_msgs
+        if isinstance(text_or_msgs, str):
+            messages = [{"role": "user", "content": text_or_msgs}]
+        assert isinstance(messages, list)
+
+        # Configure client
+        api_key = config.OPENAI_KEY
+        if config.OPENAI_API_URL is not None:
+            client = OpenAI(
+                api_key=api_key,
+                base_url=config.OPENAI_API_URL
+            )
+        else:
+            client = OpenAI(api_key=api_key)
+
+        # Send request to chat completions API
+        # Temperature will be set to 0 for deterministic output (i.e., greedy decoding)
+        stream = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+        )
+        if not stream.choices[0].message.content:
+            raise ValueError("The response from the API is NULL or an empty string!")
+        response = stream.choices[0].message.content
+    except Exception as e:
+        print(e)
+        return None
+    return response
+
+
+def prepare_llm_eval_prompts(data, task=None, llm_input_col="res"):
+    """
+    Prepare evaluation prompts for a given task using LLM-generated responses.
+
+    This function formats prompts for language model evaluation by either 
+    filling in placeholders within a template prompt with data from each row 
+    or by appending the LLM response to a fixed prompt template.
+
+    Parameters
+    ----------
+    data : list of dict
+        The dataset containing LLM-generated responses to be evaluated.
+    task : str, optional
+        The name of the task for which prompts need to be prepared. This is 
+        used to fetch the corresponding prompt template and mappings from 
+        the configuration.
+    llm_input_col : str, optional
+        The column name in the data dict that contains the input text for 
+        the LLM. Defaults to "res".
+
+    Returns
+    -------
+    list of str
+        A list of formatted prompts ready for evaluation.
+    """
+    # Set up prompt formatters
+    # If prompt contains row formatters, then fill them in with row information
+    task_prompt_dict = config.TASK_TO_PROMPT_DICT.get(task, {})
+    use_prompt_formatter = "mapping" in task_prompt_dict
+
+    # Prepare prompts
+    # CASE 1: Prompt contains string formatters
+    prompts = []
+    if use_prompt_formatter:
+        replace_dict = task_prompt_dict.get('mapping', {})
+        prompt = task_prompt_dict.get('prompt', '')
+        for row in data:
+            single_prompt = prompt
+            for k, v in replace_dict.items():
+                # CASE 1: If "res" was specified, but LLM input column is different
+                #         then convert
+                if v == "res" and llm_input_col != "res":
+                    val = row[llm_input_col]
+                # CASE 2: Any other column
+                else:
+                    val = row[v]
+                single_prompt = single_prompt.replace(k, str(val))
+            prompts.append(single_prompt)
+    # CASE 2: Otherwise, simply append LLM response to end of prompt
+    else:
+        LOGGER.debug("[ChatGPT Evaluator] Concatenating LLM response to prompt")
+        prompt = task_prompt_dict.get('prompt', '')
+        prompts = [prompt + item[llm_input_col] for item in data]
+
+    return prompts
